87f8193da56461a0d0308bcbcdebe3f705f6dae0
savethumbnail <85308391+savethumbnail@users.noreply.github.com>
2022-08-13 17:45:59 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 
 
-picom -f &
+#picom -f &
 nitrogen --restore
 xsetroot -cursor_name left_ptr
-exec bspwm
-#run nm-applet &
-#dbus-run-session bspwm
+#exec bspwm
+run nm-applet &
+dbus-run-session bspwm
